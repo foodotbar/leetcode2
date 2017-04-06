5b6536a68aa99dfbcf052f7ab948a3dbc374b581
foodotbar <foodotbar@126.com>
2017-04-06 20:54:18 +0800

    unsolved 18 4Sum

--- 18.4Sum.cpp
+++ 18.4Sum.cpp
@@ -3,30 +3,29 @@ public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
         int numsSize=nums.size();
         vector<vector<int>> ret;
-        sort(nums.begin(),nums.end());
-        if(numsSize<=4)
+        if(numsSize<4)
             return ret;
-        for(int i=0;i<numsSize-3;i++){
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<numsSize-3;i++)
+        {
             if(i>0&&nums[i]==nums[i-1]) continue;
-            for(int j=i+1;j<numsSize-2;){
-                int left=j+1;
-                int right=numsSize-1;
-                while(left<right){
-                    vector<int> curr;
-                    if(nums[i]+nums[j]+nums[left]+nums[right]==target){
-                        curr.push_back(nums[i]);
-                        curr.push_back(nums[j]);
-                        curr.push_back(nums[left]);
-                        curr.push_back(nums[right]);
-                        ret.push_back(curr);
-                        do{left++;}while(nums[left]=nums[left-1]&&left<right);
-                        do{right--;}while(nums[right]==nums[right-1]&&left<right);
-                    }
-                    if(nums[i]+nums[j]+nums[left]+nums[right]>target){
-                        --right;
-                    }
-                    if(nums[i]+nums[j]+nums[left]+nums[right]<target){
-                        ++left;
+            if(nums[i]+nums[i+1]+nums[i+2]+nums[i+3]>target) break;
+            if(nums[i]+nums[numsSize-3]+nums[numsSize-2]+nums[numsSize-1]<target) continue;
+            for(int j=i+1;j<numsSize-2;)
+            {
+                if(j>i+1&&nums[j]==nums[j-1]) continue;
+                if(nums[i]+nums[j]+nums[j+1]+nums[j+2]>target) break;
+                if(nums[i]+nums[j]+nums[numsSize-2]+nums[numsSize-1]<target) continue;
+                int left=j+1,right=numsSize-1;
+                while(left<right)
+                {
+                    int sum=nums[i]+nums[j]+nums[left]+nums[right];
+                    if(sum<target) left++;
+                    else if(sum>target) right--;
+                    else{
+                        ret.push_back(vector<int>{nums[i],nums[j],nums[left],nums[right]});
+                        do{left++;}while(nums[left]==nums[left-1]&&left<right);
+                        do{right--;}while(nums[right]==nums[right+1]&&left<right);
                     }
                 }
             }
