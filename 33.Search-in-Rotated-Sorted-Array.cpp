1f2b70d0d7a8e9b9302a0c27b5e5b421fb659686
foodotbar <foodotbar@126.com>
2017-04-09 19:33:11 +0800

    solved 33 Search in Rotated Sorted Array

---
+++ 33.Search-in-Rotated-Sorted-Array.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int n=nums.size();
+        int lo=0,hi=n-1;
+        // find the index of the smallest value using binary search.
+        // Loop will terminate since mid < hi, and lo or hi will shrink by at least 1.
+        // Proof by contradiction that mid < hi: if mid==hi, then lo==hi and loop would have been terminated.
+        while(lo<hi){
+            int mid=(lo+hi)/2;
+            if(nums[mid]>nums[hi]) lo=mid+1;
+            else hi=mid;
+        }
+        // lo==hi is the index of the smallest value and also the number of places rotated.
+        int rot=lo;
+        lo=0;hi=n-1;
+        // The usual binary search and accounting for rotation.
+        while(lo<=hi){
+            int mid=(lo+hi)/2;
+            int realmid=(mid+rot)%n;
+            if(nums[realmid]==target)return realmid;
+            if(nums[realmid]<target)lo=mid+1;
+            else hi=mid-1;
+        }
+        return -1;
+    }
+};
+
+/*
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        if(nums.size()<1)
+            return -1;
+        for(int i=0;i<nums.size();i++)
+            if(nums[i]==target)
+                return i;
+        return -1;
+    }
+};
+
+*/
