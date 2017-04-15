275eb2daf41138a45388c579fc86106d40f608c6
foodotbar <foodotbar@126.com>
2017-04-15 21:14:12 +0800

    solved 82 Remove Duplicates from Sorted List II by recursive solution

--- 82.Remove-Duplicates-from-Sorted-List-II.cpp
+++ 82.Remove-Duplicates-from-Sorted-List-II.cpp
@@ -18,3 +18,24 @@ public:
         return start->next;
     }
 };
+
+/*
+
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        if(head==NULL ||head->next==NULL)
+            return head;
+        ListNode* p = head->next;
+        if(p->val != head->val){
+            head->next=deleteDuplicates(p);
+            return head;
+        }else{
+            while(p && p->val== head->val )
+                p=p->next;
+            return deleteDuplicates(p);
+        }
+    }
+};
+
+*/
