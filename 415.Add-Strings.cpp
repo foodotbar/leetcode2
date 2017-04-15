84b842909aa3213a8b519f470e7dc5dedac8a96b
foodotbar <foodotbar@126.com>
2017-04-15 11:08:26 +0800

    delete redundant if-else

--- 415.Add-Strings.cpp
+++ 415.Add-Strings.cpp
@@ -3,13 +3,7 @@ public:
     string addStrings(string num1, string num2) {
         int n1 = num1.length(), n2 = num2.length(), sum = 0;
         string ret;
-        if( n1 < 1 && n2 <1 )
-            return ret;
-        if( n1 < 1 )
-            return num2;
-        if( n2 < 1 )
-            return num1;
-        while( n1 >0 || n2 > 0){
+        while( n1 >0 || n2 > 0 ){
             int tmp1 = (n1-1) >=0 ? num1[n1-1] - '0' : 0;
             int tmp2 = (n2-1) >=0 ? num2[n2-1] - '0' : 0;
             sum += tmp1 + tmp2;
