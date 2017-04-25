1ad1a1c6efe99deeca20fb10f161d61492a586b6
foodotbar <foodotbar@126.com>
2017-04-25 20:56:38 +0800

    solved 404 Sum of Left Leaves by iteration way

--- 404.Sum-of-Left-Leaves.cpp
+++ 404.Sum-of-Left-Leaves.cpp
@@ -18,3 +18,29 @@ public:
             
     }
 };
+
+/*
+
+class Solution {
+public:
+    int sumOfLeftLeaves(TreeNode* root) {
+        if(!root||!root->left&&!root->right)
+            return 0;
+        queue<TreeNode*> q;
+        q.push(root);
+        int ret=0;
+        while(!q.empty()){
+            TreeNode* node=q.front();
+            q.pop();
+            if(node->left&&!node->left->left&&!node->left->right)
+                ret+=node->left->val;
+            if(node->left)
+                q.push(node->left);
+            if(node->right)
+                q.push(node->right);
+            }
+        return ret;
+    }
+};
+
+*/
