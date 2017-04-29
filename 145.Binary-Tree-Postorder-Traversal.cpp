8b5c64c1dce94b11ec56f25c9081ec90a249adba
foodotbar <foodotbar@126.com>
2017-04-29 20:30:12 +0800

    solved 145 Binary Tree Postorder Traversal by non-recursive/iterative way

--- 145.Binary-Tree-Postorder-Traversal.cpp
+++ 145.Binary-Tree-Postorder-Traversal.cpp
@@ -22,3 +22,33 @@ public:
         ret.push_back(root->val);
     }
 };
+
+/**** non-recursive way
+
+class Solution {
+public:
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int> ret;
+        if(!root)
+            return ret;
+        stack<TreeNode*> s;
+        s.push(root);
+        TreeNode* head=root;
+        while(!s.empty()){
+            TreeNode* top=s.top();
+            if((!top->left&&!top->right)||top->right==head||top->left==head){
+                ret.push_back(top->val);
+                s.pop();
+                head=top;
+            }else{
+                if(top->right)
+                    s.push(top->right);
+                if(top->left)
+                    s.push(top->left);
+            }
+        }
+        return ret;
+    }
+};
+
+*/
