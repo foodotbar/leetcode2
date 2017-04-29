5ba177f7cd5c190c00de513b1fe9584f64560cec
foodotbar <foodotbar@126.com>
2017-04-29 17:43:13 +0800

    solved 198 House Robber by dp

--- 198.House-Robber.cpp
+++ 198.House-Robber.cpp
@@ -31,3 +31,20 @@ public:
 };
 
 */
+
+/*
+
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int a=0,b=0;
+        for(int i=0;i<nums.size();i++){
+            int m=a,n=b;
+            a=n+nums[i];
+            b=max(m,n);
+        }
+        return max(a,b);
+    }
+};
+
+*/
