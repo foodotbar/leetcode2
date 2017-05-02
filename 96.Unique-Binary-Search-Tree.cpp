e5078138a56533d027413625d4b871a76b40c0d0
foodotbar <foodotbar@126.com>
2017-05-03 07:21:38 +0800

    solved 96 Unique Binary Search Tree by Catalan Number theory

---
+++ 96.Unique-Binary-Search-Tree.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int numTrees(int n) {
+        vector<int> dp(n+1,0);
+        dp[0]=1;
+        dp[1]=1;
+        for(int i=2;i<=n;i++){
+            for(int j=0;j<i;j++){
+                dp[i]+=dp[j]*dp[i-j-1];
+            }
+        }
+        return dp[n];
+    }
+};
+
+/** Catalan Number
+ * Taking 1~n as root respectively:
+ *      1 as root: # of trees = F(0) * F(n-1)  // F(0) == 1
+ *      2 as root: # of trees = F(1) * F(n-2) 
+ *      3 as root: # of trees = F(2) * F(n-3)
+ *      ...
+ *      n-1 as root: # of trees = F(n-2) * F(1)
+ *      n as root:   # of trees = F(n-1) * F(0)
+ *
+ * So, the formulation is:
+ *      F(n) = F(0) * F(n-1) + F(1) * F(n-2) + F(2) * F(n-3) + ... + F(n-2) * F(1) + F(n-1) * F(0)
+ */
