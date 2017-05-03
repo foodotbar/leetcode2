9f28c27a52b3a026f0d545afa2b5983e2b1cbb9a
foodotbar <foodotbar@126.com>
2017-05-03 21:07:45 +0800

    solved 337 House Robber III

--- 337.House-Robber-III.cpp
+++ 337.House-Robber-III.cpp
@@ -10,37 +10,6 @@
 class Solution {
 public:
     int rob(TreeNode* root) {
-        if(!root)
-            return 0;
-        vector<int> ret;
-        traversal(root,0,ret);
-        int maxOdd=0,maxEven=0;
-        for(int i=0;i<ret.size();i++){
-            if(i%2==0)
-                maxOdd+=ret[i];
-            else
-                maxEven+=ret[i];
-        }
-        return max(maxOdd,maxEven);
-    }
-    void traversal(TreeNode* root,int level,vector<int> &ret){
-        if(!root)
-            return;
-        if(level==ret.size())
-            ret[level]=0;
-        ret[level]+=root->val;
-        if(root->left)
-            traversal(root->left,level+1,ret);
-        if(root->right)
-            traversal(root->right,level+1,ret);
-    }
-};
-
-/*
-
-class Solution {
-public:
-    int rob(TreeNode* root) {
         int l=0,r=0;
         return tryRob(root,l,r);
     }
@@ -51,6 +20,34 @@ public:
         l=tryRob(root->left,ll,lr);
         r=tryRob(root->right,rl,rr);
         return max(root->val+ll+lr+rr+rl,l+r);
+    }
+};
+
+/* incorrect answer
+
+class Solution {
+public:
+    int rob(TreeNode* root) {
+        if(!root)
+            return 0;
+        vector<int> ret;
+        traversal(root,0,ret);
+        int a=0,b=0;
+        for(int i=0;i<ret.size();i++){
+            int m=a,n=b;
+            a=n+ret[i];
+            b=max(m,n);
+        }
+        return max(a,b);
+    }
+    void traversal(TreeNode* root,int level,vector<int> &ret){
+        if(!root)
+            return;
+        if(level==ret.size())
+            ret.push_back(0);
+        ret[level]+=root->val;
+        traversal(root->left,level+1,ret);
+        traversal(root->right,level+1,ret);
     }
 };
 
