c992736488a6b0d5f2a19e4fc74fef3c902b6592
foodotbar <foodotbar@126.com>
2017-05-03 20:05:41 +0800

    solved 95 Unique Binary Search Tree II

--- 95.Unique-Binary-Search-Tree-II.cpp
+++ 95.Unique-Binary-Search-Tree-II.cpp
@@ -35,3 +35,36 @@ public:
         return ret;
     }
 };
+
+/*
+
+class Solution {
+public:
+    vector<TreeNode*> generateTrees(int n) {
+        if(n==0)
+            return {};
+        return createTree(1,n);
+    }
+    vector<TreeNode*> createTree(int left,int right){
+        vector<TreeNode*> ret;
+        if(left>right){
+            ret.push_back(NULL);
+            return ret;
+        }
+        for(int k=left;k<=right;k++){
+            vector<TreeNode*> leftTree=createTree(left,k-1);
+            vector<TreeNode*> rightTree=createTree(k+1,right);
+            for(int i=0;i<leftTree.size();i++){
+                for(int j=0;j<rightTree.size();j++){
+                    TreeNode* root=new TreeNode(k);
+                    root->left=leftTree[i];
+                    root->right=rightTree[j];
+                    ret.push_back(root);
+                }
+            }
+        }
+        return ret;
+    }
+};
+
+*/
