15e3ebcd24cca965808f16d1552d0aa309b50911
foodotbar <foodotbar@126.com>
2017-05-04 23:35:58 +0800

    solved 103 Binary Tree Zigzag Level Order Traversal by recursive way

--- 103.Binary-Tree-Zigzag-Level-Order-Traversal.cpp
+++ 103.Binary-Tree-Zigzag-Level-Order-Traversal.cpp
@@ -66,3 +66,28 @@ public:
 };
 
 */
+
+/*
+
+class Solution {
+public:
+    vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
+        vector<vector<int>> result;
+        traverse(root,0,result,true);
+        return result;
+    }
+    void traverse(TreeNode *root,int level,vector<vector<int>> &result,bool left_to_right){
+        if (!root) 
+            return;
+        if (level==result.size())
+            result.push_back({});
+        if (left_to_right)
+            result[level].push_back(root->val);
+        else
+            result[level].insert(result[level].begin(), root->val);
+        traverse(root->left,level+1,result,!left_to_right);
+        traverse(root->right,level+1,result,!left_to_right);
+    }
+};
+
+*/
