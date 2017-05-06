7b002f9277d5d122078bd3553f8bb3e7b97fc5a5
foodotbar <foodotbar@126.com>
2017-05-06 21:37:42 +0800

    solved 99 Recover Binary Search Tree by recursive way with 3 pointers with explains

--- 99.Recover-Binary-Search-Tree.cpp
+++ 99.Recover-Binary-Search-Tree.cpp
@@ -52,3 +52,34 @@ public:
 };
 
 */
+
+/*
+
+class Solution {
+    TreeNode* first=NULL;
+    TreeNode* second=NULL;
+    TreeNode* pre=NULL;
+public:
+    void recoverTree(TreeNode* root) {
+        inorder(root);
+        swap(first->val,second->val);
+    }
+    void inorder(TreeNode* root){
+        if(!root)
+            return;
+        inorder(root->left); //search the left sub-tree
+        //in inorder traversal of BST, prev should always have smaller value than current value
+        if(pre!=NULL&&pre->val>=root->val){
+             //incorrect smaller node is always found as prev node
+            if(first==NULL)
+                first=pre;
+            //incorrect larger node is always found as curr(root) node
+            second=root;
+        }
+        // update the prev node
+        pre=root;
+        inorder(root->right); //search the right sub-tree
+    }
+};
+
+*/
