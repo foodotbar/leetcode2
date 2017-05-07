04f6aa26044d7a49bc08b2af49bc39aa6ef5e764
foodotbar <foodotbar@126.com>
2017-05-08 07:53:28 +0800

    solved 55 Jump Game from the last

--- 55.Jump-Game.cpp
+++ 55.Jump-Game.cpp
@@ -7,3 +7,18 @@ public:
         return i==nums.size();
     }
 };
+
+/*
+
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int last=nums.size()-1,i,j;
+        for(i=nums.size()-1;i>=0;i--)
+            if(i+nums[i]>=last)
+                last=i;
+        return last<=0;
+    }
+};
+
+*/
