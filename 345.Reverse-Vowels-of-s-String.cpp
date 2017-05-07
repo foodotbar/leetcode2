0b557e473485c606c3007e31d698833bd3e2c9f9
foodotbar <foodotbar@126.com>
2017-05-07 19:35:19 +0800

    solved 345 Reverse Vowels of a String

---
+++ 345.Reverse-Vowels-of-s-String.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    string reverseVowels(string s) {
+        for(int start=0,end=s.length()-1;start<end;){
+            while(!isVowels(s[start])&&start<end)
+                start++;
+            while(!isVowels(s[end])&&start<end)
+                end--;
+            if(isVowels(s[start])&&isVowels(s[end]))
+                swap(s[start++],s[end--]);
+        }
+        return s;
+    }
+    bool isVowels(char c){
+        switch(c){
+            case 'a':return true;
+            case 'e':return true;
+            case 'i':return true;
+            case 'o':return true;
+            case 'u':return true;
+            case 'A':return true;
+            case 'E':return true;
+            case 'I':return true;
+            case 'O':return true;
+            case 'U':return true;
+            default:return false;
+        }
+    }
+};
