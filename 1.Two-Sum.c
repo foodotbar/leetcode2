fb234f6f3f3acc45871408515392acae7accbed9
foodotbar <foodotbar@126.com>
2017-06-11 09:37:51 +0800

    solved 1 Two Sum by cpp

--- 1.Two-Sum.c
+++ 1.Two-Sum.c
@@ -16,3 +16,27 @@ int* twoSum(int* nums, int numsSize, int target) {
     }
     return ptr;
 }
+
+/*
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> ans;
+        if(nums.size()<2)
+            return ans;
+        unordered_map<int,int> m;
+        for(int i=0;i<nums.size();i++)
+            m[nums[i]]=i;
+        for(int i=0;i<nums.size();i++){
+            int t=target-nums[i];
+            if(m.count(t)&&m[t]>i){
+                ans.push_back(i);
+                ans.push_back(m[t]);
+            }
+        }
+        return ans;
+    }
+};
+
+*/
