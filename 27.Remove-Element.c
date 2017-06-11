d78e97bf2b15c51014cf55114a9f839a3d299c27
foodotbar <foodotbar@126.com>
2017-06-11 08:42:22 +0800

    solved 27 Remove Element by cpp

--- 27.Remove-Element.c
+++ 27.Remove-Element.c
@@ -8,3 +8,117 @@ int removeElement(int* nums, int numsSize, int val) {
     numsSize=j;
     return j;
 }
+
+/*
+
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int index=-1;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]!=val)
+                nums[++index]=nums[i];
+        }
+        return index+1;
+    }
+};
+
+*//*
+
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int index=-1;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]!=val)
+                nums[++index]=nums[i];
+        }
+        return index+1;
+    }
+};
+
+*//*
+
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int index=-1;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]!=val)
+                nums[++index]=nums[i];
+        }
+        return index+1;
+    }
+};
+
+*//*
+
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int index=-1;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]!=val)
+                nums[++index]=nums[i];
+        }
+        return index+1;
+    }
+};
+
+*//*
+
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int index=-1;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]!=val)
+                nums[++index]=nums[i];
+        }
+        return index+1;
+    }
+};
+
+*//*
+
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int index=-1;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]!=val)
+                nums[++index]=nums[i];
+        }
+        return index+1;
+    }
+};
+
+*//*
+
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int index=-1;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]!=val)
+                nums[++index]=nums[i];
+        }
+        return index+1;
+    }
+};
+
+*//*
+
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int index=-1;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]!=val)
+                nums[++index]=nums[i];
+        }
+        return index+1;
+    }
+};
+
+*/
